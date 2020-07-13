d3242b0100da3d2f89ffae0efb972726c08e98f9
Mauricio Robayo <rfmajo@gmail.com>
2020-07-13 09:56:20 -0500

    chore: add state to class component

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 import Message from './Message'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Message name='Manny' message='This is a quick message!!!'/>
-      </header>
-    </div>
-  );
+const initialState = {
+  name: 'Manny',
+  message: 'React is cool',
+}
+
+type State = Readonly<typeof initialState>
+
+class App extends React.Component<any, State> {
+    readonly state: State = initialState;  
+    render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Message name={this.state.name} message={this.state.message} />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
